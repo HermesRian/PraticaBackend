29fad2f0e6f7be5f45c1c8abd8e6c11e536e56ae
hermes <hermes.silva@ffa.com.py>
2025-11-06 17:41:46 -0300

    feat(CORS): aplicação ajustada para receber PATCH do frontend

--- src/main/java/com/cantina/SistemaCantinaApplication.java
+++ src/main/java/com/cantina/SistemaCantinaApplication.java
@@ -9,17 +9,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class SistemaCantinaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SistemaCantinaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaCantinaApplication.class, args);
+    }
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/**").allowedOrigins("http://localhost:5173");
-			}
-		};
-	}
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
